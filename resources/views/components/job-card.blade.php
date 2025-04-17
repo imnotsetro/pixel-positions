37e86a8a9462572428438355b8b0a8e91122bde1
imnotsetro <matias.abreg@gmail.com>
2025-04-17 20:01:42 -0300

    Essentials blade components created

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Title</div>
+
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - Form $60,000</p>
+    </div>
+
+    <div>
+        <div class="flex justify-between items-center mt-auto">
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+    </div>
+</div>
