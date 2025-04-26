562caa153feecaa7370849b0c2529ec4741d15aa
imnotsetro <matias.abreg@gmail.com>
2025-04-25 21:44:12 -0300

    Database created (Jobs, Tags, Employers) - Data can be viewed in the index page

---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Employer;
+use App\Models\Job;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class JobTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_belongsToAnEmployer(): void
+    {
+        $employer = Employer::factory()->create();
+        $job = Job::factory()->create([
+            'employer_id' => $employer->id,
+        ]);
+
+        $this->assertTrue($job->employer->is($employer));
+    }
+
+    public function test_canHaveTags(): void
+    {
+        $job = Job::factory()->create();
+
+        $job->tag('Frontend');
+
+        $this->assertCount(1, $job->tags);
+    }
+}
